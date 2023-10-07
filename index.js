09651d383a23ea3306ad465786e9f368f644fceb
2439905184 <2439905184@qq.com>
2023-10-07 10:56:10 +0800

    补充

--- index.js
+++ index.js
@@ -5,50 +5,40 @@ function into_menu(p_name)
 var main_menu = document.getElementsByClassName("grid-container").item(0)
 
 var item_length = document.getElementsByClassName("grid-item").length
-var item1 = main_menu.getElementsByClassName("grid-item").item(0)
-item1.addEventListener("click", function()
-{
-    into_menu("漫评")
-});
-
 for(var i = 0; i<item_length ; i++)
 {
-    var each = main_menu.getElementsByClassName("grid-item").item(i)
-    each.addEventListener("click",function()
-    {
-        if(i==0)
-        {
-            into_menu("漫评")
-        }
-        else if(i==1)
-        {
-            into_menu("文化鱼塘")
-        }
-        else if(i==2)
-        {
-            into_menu("创作者百科")
-        }
-        else if(i==3)
-        {
-            into_menu("玄幻小说")
-        }
-        else if(i==4)
-        {
-            into_menu("经典讨论")
-        }
-        else if(i==5)
-        {
-            into_menu("推荐")
-        }
-        else if(i==6)
-        {
-            into_menu("动漫哲学")
-        }
-        else if(i==7)
-        {
-            into_menu("关于动漫与天道")
-        }
-        
-    })
+    (function(index) {
+        var each = main_menu.getElementsByClassName("grid-item").item(index)
+        each.addEventListener("click", function() {
+            console.log(index);
+            // 根据索引值执行相应的代码
+            if(index == 0)
+            {
+                into_menu("漫评")
+            }
+            else if(index == 2)
+            {
+                into_menu("创作者百科")
+            }
+            else if(index == 4)
+            {
+                into_menu("经典讨论")
+            }
+            else if(index == 6)
+            {
+                into_menu("动漫哲学")
+            }
+            else if(index == 7)
+            {
+                into_menu("动漫与天道")
+            }
+            else if(index == 9)
+            {
+                into_menu("怀旧")
+            }
+        });
+    })(i);
 }
-// console.log(item1)
+
+var music_menu = document.getElementsByClassName("grid-container").item(0)
+var music_menu_length = document.getElementsByClassName("grid-container").length
